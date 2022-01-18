03a569c94e725e9086b77918b81a7690d19e1486
soham-madhav <sohamjmadhav@gmail.com>
2022-01-19 02:46:36 +0530

    analysis page redo 1

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,107 +1,87 @@
 import React from 'react';
 import NavigationBar from "../Navbar/Navbar";
-import { Card, Col, Container, Row, Tabs, Tab } from 'react-bootstrap'
+import {Card, Col, Container, Row, Tabs, Tab} from 'react-bootstrap'
 import WeeklyChart from './charts/WeeklyChart.js'
 import QuaterlyChart from './charts/QuaterlyChart';
 import MonthlyChart from './charts/MonthlyChart';
-import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import './dashboardStyle.css'
+
+
 
 const Dashboard = () => {
     return (
         <div>
             <NavigationBar />
             <Container >
-                <Row><h2>Analysis</h2></Row>
+                <Row style={{padding:'10px 10px 10px 0px'}}><h2>Analysis</h2></Row>
                 <Row className="row justify-content-md-center">
-                    <Col xs={12} md={4} lg={3}>
+                <Col xs={12} md={4} lg={3}>
                         <Row>
-                            <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
+                            <Card style={{ height: 'fit-content', width: '100%', marginBottom: '30px', padding: '20px' }}>
                                 <h5>Income:</h5>
-                                <ProgressBar now={80} />
+                                <ProgressBar variant="info" now={80} />
                             </Card>
                         </Row>
                         <Row>
-                            <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
+                            <Card style={{ height: 'fit-content', width: '100%', marginBottom: '30px', padding: '20px' }}>
                                 <h5>Expense:</h5>
-                                <ProgressBar now={80} />
+                                <ProgressBar variant="info"  now={80} />
                             </Card>
                         </Row>
                         <Row>
-                            <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
+                            <Card style={{ height: 'fit-content', width: '100%', marginBottom: '30px', padding: '20px' }}>
                                 <h5>Budget:</h5>
-                                <ProgressBar now={80} />
+                                <ProgressBar variant="info" now={80} />
                             </Card>
                         </Row>
                         <Row>
-                            <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
+                            <Card style={{ height: 'fit-content', width: '100%', marginBottom: '30px', padding: '20px' }}>
                                 <h5>Income:</h5>
-                                <ProgressBar now={80} />
+                                <ProgressBar variant="info" now={80} />
                             </Card>
                         </Row>
                     </Col>
                     <Col xs={12} md={8} lg={9}>
-                        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
-                            <Tab eventKey="home" title="Home">
-                              <h2>tab 1</h2>
+                        <Tabs  defaultActiveKey="weekly" id="uncontrolled-tab-example" className="tab">
+                            <Tab eventKey="weekly" title="Weekly"  >
+                                <Row>
+                                    <Col md={12} lg={12} className="justify-content-center" >
+                                    <Card className="justify-content-center" style={{width:'100%', backgroundColor:'transparent'}}> 
+                                        <WeeklyChart />
+                                    </Card>
+                                    </Col> 
+                                </Row>
+                                
+                              
+                              
                             </Tab>
-                            <Tab eventKey="profile" title="Profile">
-                                <h2>tab 2</h2>
+                            <Tab eventKey="monthly" title="Monthly">
+                                <Row>
+                                    <Col md={12} lg={12} style={{height:"350px"}} >
+                                    <Card className="justify-content-center" style={{width:'100%', backgroundColor:'transparent'}}> 
+                                        <MonthlyChart />
+                                    </Card>
+                                    </Col> 
+                                </Row>
                             </Tab>
-                            <Tab eventKey="contact" title="Contact" >
-                                <h2>tab 3</h2>
+                            <Tab eventKey="quarterly" title="Quarterly" >
+                                <Row>
+                                    <Col md={12} lg={10} >
+                                    <Card className="justify-content-center" style={{width:'100%', backgroundColor:'transparent'}}> 
+                                        <QuaterlyChart />
+                                    </Card>
+                                    </Col> 
+                                </Row>
                             </Tab>
                         </Tabs>
                     </Col>
-                    {/* <Col xs={12} md={6} lg={3} className="d-flex align-items-center justify-content-center">
-                        <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
-                            <h5>Income:</h5>
-                            <ProgressBar now={80} />
-                        </Card>
-                    </Col>
-                    <Col xs={12} md={6} lg={3} className="d-flex align-items-center justify-content-center">
-                        <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
-                            <h5>Expense:</h5>
-                            <ProgressBar now={80} />
-                        </Card>
-                    </Col>
-                    <Col xs={12} md={6} lg={3} className="d-flex align-items-center justify-content-center">
-                        <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
-                            <h5>Budget:</h5>
-                            <ProgressBar now={80} />
-                        </Card>
-                    </Col>
-                    <Col xs={12} md={6} lg={3} className="d-flex align-items-center justify-content-center">
-                        <Card style={{ height: 'fit-content', width: '250px', marginBottom: '30px', padding: '20px' }}>
-                            <h5>Invested:</h5>
-                            <ProgressBar now={80} />
-                        </Card>
-                    </Col>
 
                 </Row>
-                <br />
-                <Row className="row justify-content-md-center" >
-                    <Col xs={12} lg={4} md={6}  >
-                        <Card style={{ height: 'fit-content', width: 'fit-content', padding: '20px' }} >
-                            <h4>Weekly Analysis</h4>
-                            <WeeklyChart />
-                        </Card>
-                    </Col>
-                    <Col xs={12} lg={4} md={6} >
-                        <Card style={{ height: 'fit-content', width: 'fit-content', padding: '20px' }}>
-                            <h4>Monthly Analysis</h4>
-                            <MonthlyChart />
-                        </Card>
-                    </Col>
-                    <Col xs={12} lg={4} md={6}  >
-                        <Card style={{ height: 'fit-content', width: 'fit-content', padding: '20px' }}>
-                            <h4>Quarterly Analysis</h4>
-                            <QuaterlyChart />
-                        </Card>
-                    </Col> */}
-                </Row>
+            
             </Container>
+            
+            
         </div>
     )
 }
--- frontend/src/components/Dashboard/charts/WeeklyChart.js
+++ frontend/src/components/Dashboard/charts/WeeklyChart.js
@@ -2,8 +2,23 @@ import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import { useState } from 'react';
+import {    CategoryScale, LinearScale,
+    BarElement, Title, PointElement, LineElement } from 'chart.js';
+import { Bar, Line } from 'react-chartjs-2';
+import {Row, Col} from 'react-bootstrap'
+
+
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+    CategoryScale, LinearScale, BarElement,
+    Title, Tooltip, Legend );
+ChartJS.register(
+    CategoryScale, LinearScale, PointElement,
+    LineElement, Title, Tooltip, Legend );
+    
+
 
 const WeeklyChart = () => {
     const [foodCount, setfoodCount] = useState(4);
@@ -12,36 +27,129 @@ const WeeklyChart = () => {
     const [randomCount, setrandomCount] = useState(2);
     const [dailyCount, setdailyCount] = useState(10)
 
-    const data = {
-        labels: ['Food', 'Entertainment', 'Medical', 'Random', 'Daily'],
+    const piedata = {
+        labels: ['Food', 'Entertainment', 'Medical', 'Random', 'Transport'],
         datasets: [
             {
                 label: '# of Votes',
                 data: [foodCount, entertainmentCount, medicalCount, randomCount, dailyCount],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
+                    'rgba(1, 83, 99,1)',
+                    'rgba(3, 143, 166,1)',
+                    'rgba(13, 202, 240,1)',
+                    'rgba(35, 186, 217,1)',
+                    'rgba(109, 203, 222,1)',
+                    'rgba(181, 226, 235,0.6)',
                 ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
+                borderWidth: 0,
             },
         ],
     };
 
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Bar Chart',
+          },
+        },
+      };
+
+
+    const bardata = {
+        labels: ["Sat","Sun","Mon","Tue","Wed","Thur","Fri"],
+        datasets: [
+          {
+            label: 'Food',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+          {
+            label: 'Entertainment',
+            data: [65, 9, 90, 81, 56, 55, 40],
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+          {
+            label: 'Medical',
+            data: [65, 9, 90, 81, 56, 55, 40],
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },{
+            label: 'Random',
+            data: [65, 9, 90, 81, 56, 55, 40],
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },{
+            label: 'Transport',
+            data: [65, 9, 90, 81, 56, 55, 40],
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+        ]
+    }
+
+    const linedata = {
+        labels:["Sat","Sun","Mon","Tue","Wed","Thur","Fri"],
+        datasets: [
+          {
+            label: 'Food',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#ccc774',
+            tension: 0.1
+          }, 
+          {
+            label: 'Entertainment',
+            data: [95, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#c48a74',
+            tension: 0.1
+          },{
+            label: 'Random',
+            data: [25, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#112774',
+            tension: 0.1
+          },{
+            label: 'Medical',
+            data: [55, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#74a774',
+            tension: 0.1
+          },
+          {
+            label: 'Transport',
+            data: [50, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#742774',
+            tension: 0.1
+          }
+        ]
+    }
+
     return (
         <div>
-            <Doughnut data={data} />;
+        <Row>
+            <Col lg={6} md={12}>
+                <Doughnut data={piedata} />;
+            </Col>
+            <Col lg={6} md={12}>
+                <Bar options={options} data={bardata} height="300px" />;
+            </Col>
+            <br />
+            <hr />
+            <br />
+            <Row>
+                <Col lg={12} md={12}>
+                    <Line options={options} data={linedata} />;
+                </Col>
+
+            </Row>
+            
+            
+        </Row>
+            
         </div>
     )
 }
